e5abcdb1857190745ef7575e511f1ea3d108624d
Naohiro Aota <naota@elisp.net>
2014-01-25 03:45:22 +0900

    s/litecoin/monacoin/

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Monacoins");
+    case mBTC: return QString("Milli-Monacoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Monacoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
